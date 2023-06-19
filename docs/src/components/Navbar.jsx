48c3079245d37fb525ab3f4cfcf6c08a87ecb21c
itsbluejelly <ronniedenzel0@gmail.com>
2023-06-20 01:27:03 +0300

    added DOCTYPE

--- docs/src/components/Navbar.jsx
+++ docs/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img src="../images/react-icon-small.png" className="nav--icon" />
+            <img src="/docs/images/react-icon-small.png" alt="react-logo" title= "react-logo" className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
